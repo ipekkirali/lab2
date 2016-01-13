a5c70135d39e6104794b357adf6ccff2a60752b9
ipekkirali <ipekkirali2018@u.northwestern.edu>
2016-01-13 13:41:38 -0600

    exercise 1

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -4,21 +4,17 @@
 // Lab 2 programs
 //
 
-int illegalStatements() {
-    /* Start here! */
-    //int double = 0;
-    //double char = 2.5;
-    //char mychar = 'd'; mychar += 12;
-    //unsigned int a = -1;
-    //int pi = 3.14;
-    //short num = 1e9;
-    //string mystring = 5;
-    return 0;
-}
 
 int main() {
-    illegalStatements();
-    cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
-    cout << "Then work on the exercises and code them up." << endl;
+    int n;
+    cout << "Enter a number:\n";
+    cin >> n;
+
+    if ((n % 2) == 0) {
+        cout << " The value " << n << " is an even number";
+    }
+    else
+    cout << " The value " << n << " is an odd number";
 }
+
 
